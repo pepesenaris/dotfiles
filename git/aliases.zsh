7474a72b603620a26cef960d382a58993d9ad0ef
Jose Javier Senaris <pepe.senaris@alayacare.com>
2023-10-24 14:40:40 -0400

    Add alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,3 +5,13 @@ alias gbr="git br"
 alias gco="git co"
 alias glogo="git logo"
 alias glogf="git logf"
+
+alias gl="git log --color=always --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gb='git rev-parse --abbrev-ref HEAD'
+alias gap='git add -p'
+alias glh='gl | head'
+alias gds='git diff --staged'
+alias gdw='git diff -w -U0 --word-diff-regex=[^[:space:]]'
+alias gca='git commit --amend -C HEAD'
+alias gp='git push origin $(gb)'
+alias gpf='gp --force-with-lease'
